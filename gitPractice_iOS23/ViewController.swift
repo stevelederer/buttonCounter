f83e384d835c7d4753ebffc15245363f10e10d97
Steve Lederer <stevelederer@steves-2016-macbook-pro.local>
2018-11-30 13:50:03 -0700

    Initial Commit

---
+++ gitPractice_iOS23/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitPractice_iOS23
+//
+//  Created by Steve Lederer on 11/30/18.
+//  Copyright © 2018 Steve Lederer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
